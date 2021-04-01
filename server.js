342f22bdec16f4de992a0cfa42963114063f4165
bengbengku <anggiatbenget70@gmail.com>
2021-04-01 20:00:42 +0700

    ADD: User models dan menambahkan validation

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const app = express();
 //Connect Database
 connectDB();
 
+// init middleware
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => res.send('API Running ...'));
 
